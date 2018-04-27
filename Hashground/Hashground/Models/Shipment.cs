688acfda18e688b06ee0361c6472fd812e24f34f
TylerCode <TylerCode@users.noreply.github.com>
2018-04-26 21:01:55 -0600

    added encoding methods to the shipment

--- Hashground/Hashground/Models/Shipment.cs
+++ Hashground/Hashground/Models/Shipment.cs
@@ -8,6 +8,7 @@ namespace Hashground.Models
         public int averageTemperature;
         public string origination;
         public Vegetable vegetable; //fucked that naming up
+        private char delimiter = '|';
 
         public Shipment(long ShipmentId, int AverageTemperature, string Origination, Vegetable Vegetable)
         {
@@ -15,6 +16,23 @@ namespace Hashground.Models
             averageTemperature = AverageTemperature;
             origination = Origination;
             vegetable = Vegetable;
+        }
+
+        public string Encode()
+        {
+            return shipmentId.ToString() + delimiter + averageTemperature.ToString() + delimiter + origination + delimiter + vegetable.name + delimiter + vegetable.quantity.ToString();
+        }
+
+        public void Decode(string inputString)
+        {
+            string[] data = inputString.Split(delimiter);
+            int quantityParsed;
+
+            long.TryParse(data[0], out shipmentId);
+            int.TryParse(data[1], out averageTemperature);
+            origination = data[2];
+            int.TryParse(data[4], out quantityParsed);
+            vegetable = new Vegetable(data[3], quantityParsed);
         }
     }
 }
