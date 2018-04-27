7d3112b9de8eaf757cfbe2b8595dc18b1e808fcf
TylerCode <TylerCode@users.noreply.github.com>
2018-04-26 20:08:31 -0600

    initial skeleton

--- Hashground/Hashground/Program.cs
+++ Hashground/Hashground/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Hashground
 {
@@ -6,7 +9,21 @@ namespace Hashground
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            string pubKey = rsa.ToXmlString(false); //False gives pub key
+            string privKey = rsa.ToXmlString(true); //True give private key
+
+            
+        }
+
+        private static void EncryptText(string publicKey, string text, string fileName)
+        {
+
+        }
+
+        private static string DecryptText(string privateKey, string fileName)
+        {
+            return "";
         }
     }
 }
