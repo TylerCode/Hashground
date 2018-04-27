d23a89188002d27ed72757709600b0950b4dfc28
TylerCode <TylerCode@users.noreply.github.com>
2018-04-26 20:50:25 -0600

    added shipment model class

---
+++ Hashground/Hashground/Models/Shipment.cs
@@ -0,0 +1,20 @@
+﻿using Hashground.Classes;
+
+namespace Hashground.Models
+{
+    class Shipment
+    {
+        public long shipmentId;
+        public int averageTemperature;
+        public string origination;
+        public Vegetable vegetable; //fucked that naming up
+
+        public Shipment(long ShipmentId, int AverageTemperature, string Origination, Vegetable Vegetable)
+        {
+            shipmentId = ShipmentId;
+            averageTemperature = AverageTemperature;
+            origination = Origination;
+            vegetable = Vegetable;
+        }
+    }
+}
