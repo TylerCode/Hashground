ebd0a0e6d07db05870e866b58b1230ca2d6ec55c
TylerCode <TylerCode@users.noreply.github.com>
2018-04-26 20:50:15 -0600

    added veggie class

---
+++ Hashground/Hashground/Classes/Vegetable.cs
@@ -0,0 +1,14 @@
+﻿namespace Hashground.Classes
+{
+    class Vegetable
+    {
+        public string name;
+        public int quantity;
+
+        public Vegetable(string Name, int Quantity)
+        {
+            name = Name;
+            quantity = Quantity;
+        }
+    }
+}
