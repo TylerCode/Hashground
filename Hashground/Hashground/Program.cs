a70c2a1f197d640ae9739da7bd89017e6655af6c
TylerCode <TylerCode@users.noreply.github.com>
2018-04-26 20:13:38 -0600

    real skeleton
    I spent most of the time looking for a quote, decided to stick to the iRobot theme I got going.

--- Hashground/Hashground/Program.cs
+++ Hashground/Hashground/Program.cs
@@ -13,7 +13,9 @@ namespace Hashground
             string pubKey = rsa.ToXmlString(false); //False gives pub key
             string privKey = rsa.ToXmlString(true); //True give private key
 
-            
+            EncryptText(pubKey, "One day they'll have secrets. One day they'll have dreams.", "irobot.dat");
+
+            Console.WriteLine("Decrypted Message: {0}", DecryptText(privKey, "irobot.dat"));
         }
 
         private static void EncryptText(string publicKey, string text, string fileName)
